f47660c29d009672c83151bb4e387335a101d3fe
mitani24 <h.mitani@ashita-team.com>
2022-01-31 20:31:34 +0900

    petstore をやめる

--- src/data-sources/index.ts
+++ src/data-sources/index.ts
@@ -1,4 +1,10 @@
-export const dataSources = () => {
+import { Config } from "apollo-server-express";
+
+export type ContextType = {
+  dataSources: {};
+};
+
+export const dataSources: Config["dataSources"] = () => {
   return {
     // petshopAPI: new PetshopAPI(),
   };
--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,1 +1,10 @@
-export const resolvers = {};
+import { ContextType } from "../data-sources";
+import { Resolvers } from "../lib/generated/graphql-resolver-types";
+
+export const resolvers: Resolvers<ContextType> = {
+  Author: {},
+  IncrementTrackViewsPayload: {},
+  Mutation: {},
+  Query: {},
+  Track: {},
+};
