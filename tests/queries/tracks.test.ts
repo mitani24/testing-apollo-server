6cfd040991301b4013a4c4432c572b0f47ed8b70
mitani24 <h.mitani@ashita-team.com>
2022-02-03 13:56:17 +0900

    仮のテストを追加

--- tests/queries/tracks.test.ts
+++ tests/queries/tracks.test.ts
@@ -1,9 +1,25 @@
+import { RESTDataSource } from "apollo-datasource-rest";
 import { server } from "../../src/server";
 import { Tracks } from "../lib/generated/graphql-documents";
 
 describe("tracks", () => {
-  it("tracks を取得する", async () => {
+  it("/tracks エンドポイントが呼ばれる", async () => {
+    const get = jest.spyOn(RESTDataSource.prototype, "get");
+    get.mockResolvedValueOnce([
+      {
+        id: "track-0",
+        title: "track-0:title",
+        author_id: "author-0",
+      },
+    ]);
+    get.mockResolvedValueOnce({
+      id: "author-0",
+      name: "author-0:name",
+    });
+
     const res = await server.executeOperation({ query: Tracks });
-    expect(res).toMatchSnapshot();
+    expect(get.mock.calls.length).toBe(2);
+    expect(get.mock.calls[0][0]).toBe("/tracks");
+    expect(get.mock.calls[1][0]).toBe("/authors/author-0");
   });
 });
